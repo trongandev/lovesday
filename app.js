79bce14e731fb7ad59bb4626a109da6d6227e1f6
Trọng An <96829540+angutboiz@users.noreply.github.com>
2022-10-06 16:42:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const yourDate = new Date("2022-10-04T00:00:00"),
-music = ['ido', 'noinaycoanh', 'nguoiamphu'];
+
 
 document.addEventListener('DOMContentLoaded', function(){
       var rootTime = document.querySelector("time");
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function(){
             rootTime.textContent = `${(hrs>9)?hrs:"0"+hrs}:${(min>9)?min:"0"+min}:${(sec>9)?sec:"0"+sec}`;
       } olock();
       var timer = setInterval(function(){olock()}, 1000);
-      document.querySelector("audio").setAttribute("src", `music/${music[Math.floor(Math.random()*music.length)]}.mp3`);
+
 
       document.getElementsByTagName("body")[0].insertAdjacentHTML(
             "beforeend",
